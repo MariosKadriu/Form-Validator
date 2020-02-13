938cf3d23ece88d0398d25cd127b01156644d6c2
MariosKadriu <marios.kadriu@gmail.com>
2020-02-14 01:17:14 +0200

    Finished HTML and CSS and added some features like showing success, error messages, email validation and checking for required fields

---
+++ script.js
@@ -0,0 +1,47 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+};
+
+// Show success outline
+const showSuccess = input => {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success'
+};
+
+// Check email is valid
+const isValidEmail = email => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+// Capitalize the first letter of input
+const getFieldName = input => {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Check Required fields
+const checkRequired = inputArr => {
+  inputArr.forEach(input => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+};
+
+// Event Listeners
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  checkRequired([username, email, password, password2]);
+});
